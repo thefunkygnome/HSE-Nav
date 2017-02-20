b7559883354876c445d61f0cc00bc52f657f9bfe
Timur Guseynov <thefunkygnome@gmail.com>
2017-02-20 23:09:19 +0300

    Верстка под десктоп, анимация фона

--- js/anim.js
+++ js/anim.js
@@ -1,20 +1,34 @@
-$(function() {
-    function moveAimlessly() {
-        $("figure").each(function() {
-            $(this).animate({
-                top: Math.floor(Math.random()*2) == 1 ? "+=" + Math.floor(Math.random()*41).toString() : "-=" + Math.floor(Math.random()*50).toString(),
-                left: Math.floor(Math.random()*2) == 1 ? "+=" + Math.floor(Math.random()*41).toString() : "-=" + Math.floor(Math.random()*50).toString()
-            }, 50000);
-        });
-    }
-    moveAimlessly();
-});
+function figFloat() {
+  var a = Math.random() * 100;
+
+  $('figure').each(function() {
+      $(this).animate({
+          top: Math.floor(Math.random()*2) == 1 ? "+=" + Math.floor(Math.random()*41).toString() : "-=" + Math.floor(Math.random()*50).toString(),
+          left: Math.floor(Math.random()*2) == 1 ? "+=" + Math.floor(Math.random()*41).toString() : "-=" + Math.floor(Math.random()*50).toString(),
+      }, 3000);
+
+  });
+  figFloat()
+}
 
 $(function() {
-  $("figure").hover(function() {
-    $(this).css({
-      '-webkit-transform': 'rotate('+130+'deg)',
-      '-moz-transform': 'rotate('+130+'deg)'
-    })
-  })
+  figFloat()
 })
+
+// this will be called onload
+// jQuery(function($) {
+//     $('figure').each(function() {
+//         var a = Math.random() * 100;
+//         $(this).css('transform', 'rotate(' + a + 'deg)');
+//     }, 3000);
+// });
+
+//
+// $(function() {
+//   $("figure").hover(function() {
+//     $(this).css({
+//       '-webkit-transform': 'rotate('+130+'deg)',
+//       '-moz-transform': 'rotate('+130+'deg)'
+//     })
+//   })
+// })
--- js/nav.js
+++ js/nav.js
@@ -1,74 +1,82 @@
-$(document).ready(function(){
-    $('#aud').keyup( function() {
-        var $this = $(this);
-        if($this.val().length > 3)
-            $this.val($this.val().substr(0, 3));
-    });
-     });
-
 function get_value_1() {
-  console.log('Поднимись на' + $('#aud').val().substr(0, 1) + 'этаж')
+  console.log('Поднимись на' + ' ' + $('#aud').val().substr(0, 1) + ' ' + 'этаж')
 }
 
-function get_value_2() {
-  var inputValue = $('#aud').val()
-  var valueLenght = inputValue.length
-
-  console.log(inputValue.substr(valueLenght - 1))
-  console.log(inputValue.substr(valueLenght - 1) <= 6)
-
-  if (inputValue.substr(valueLenght - 1) <= 6) {
+function get_value_2exp() {
+  console.log( $('#aud').val().substr(1, 1) )
+  if ($('#aud').val().substr(1, 1) <= 6) {
     console.log('Поверни направо')
   } else {
     console.log('Поверни налево')
   }
 }
 
+// function get_value_2() {
+//   var inputValue = $('#aud').val()
+//   var valueLength = inputValue.length
+//
+//   console.log(inputValue.substr(valueLength * 0,1))
+//   console.log(inputValue.substr(valueLength - 1) <= 6)
+//
+//   if (inputValue.substr(valueLength - 1) <= 6) {
+//     console.log('Поверни направо')
+//   } else {
+//     console.log('Поверни налево')
+//   }
+// }
+
 $(function() {
   $('#nav-btn').click(function() {
     get_value_1()
-    get_value_2()
+    get_value_2exp()
     $('#nav-txt-1').text('Поднимись на' + ' '+ $('#aud').val().substr(0, 1) + ' ' + 'этаж')
-    var inputValue = $('#aud').val()
-    var valueLenght = inputValue.length
-    if (inputValue.substr(valueLenght - 1) <= 6) {
+
+    if ($('#aud').val().substr(1, 1) <= 6) {
       $('#nav-txt-2').text('Поверни направо')
     } else {
       $('#nav-txt-2').text('Поверни налево')
     }
 
-    $('.route').show()
     $('#aud').hide()
     $('#nav-btn').hide()
     $('#main').hide()
-    $('#back-nav').show()
     $('.about').hide()
     $('.hselogo').hide()
+    $('.route').show()
+    $('#back-nav').show()
   })
 })
 
 $(function(){
   $('#back-nav').click(function() {
     $('.route').hide()
+    $('#back-nav').hide()
+    $('.about-txt-1').hide()
+    $('.about-txt-2').hide()
     $('#aud').show()
     $('#nav-btn').show()
     $('#main').show()
-    $('#back-nav').hide()
     $('.about').show()
-    $('.about-txt-1').hide()
-    $('.about-txt-2').hide()
     $('.hselogo').show()
   })
 })
 
 $(function() {
   $('.about').click(function() {
-    $('.about-txt-1').show()
-    $('.about-txt-2').show()
     $('#aud').hide()
     $('#nav-btn').hide()
     $('#main').hide()
-    $('#back-nav').show()
     $('.about').hide()
+    $('.about-txt-1').show()
+    $('.about-txt-2').show()
+    $('#back-nav').show()
   })
 })
+
+$(function(event) {
+  $("#aud").keyup(function(event) {
+    if (event.keyCode == 13) {
+      $("#nav-btn").click();
+    }
+  });
+});
